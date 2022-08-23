bf00ab3ba9d65bc9655da77bdbcb5745c5303426
Alper Fırat Aykan <74792755+alperaykan@users.noreply.github.com>
2022-08-23 19:24:14 +0300

    Başladık
    Başlangıcı yaptım sanırım

---
+++ ConsoleApps/HelloWorlds/HelloWorlds/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorlds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Başlangıç
+            
+             Console.WriteLine("Hello World!");
+             
+             int j = 10;
+
+             for(int i= 0; i < j; i++)
+             {
+                 Console.WriteLine("Selamın aleyküm");
+             }
+
+            #endregion
+        }
+    }
+}
