ad78f60baa06d27d03b8504dd0c284b017e5e759
Alper Fırat Aykan <74792755+alperaykan@users.noreply.github.com>
2022-08-24 18:41:57 +0300

    Karar Mekanizmaları
    İf else, switch case örnekleri

---
+++ ConsoleApps/KararMekanizmaları/KararMekanizmaları/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace KararMekanizmaları
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region SwitchCase
+
+            //Console.WriteLine("Hangi meyveyi almak istiyorsunuz?");
+            //string fruit = Console.ReadLine();
+
+            //switch (fruit)
+            //{
+            //    case "elma":
+            //        Console.WriteLine("Bir elma aldınız. Borcunuz 5 TL");
+            //        break; 
+            //    case "armut":
+            //        Console.WriteLine("Bir armut aldınız. Borcunuz 12 TL");
+            //        break;  
+            //    case "ananas":
+            //        Console.WriteLine("Bir ananas aldınız. Borcunuz 56 TL");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Borcunuz 40 tl");
+            //        break;
+            //}
+
+
+            #endregion
+        }
+    }
+}
