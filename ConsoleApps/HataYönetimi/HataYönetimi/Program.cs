ba196cb3840ca194231beb06acaa360bac1e3f5e
Alper Fırat Aykan <74792755+alperaykan@users.noreply.github.com>
2022-08-24 17:49:30 +0300

    TryCatch
    hata yönetimi denemeleri

--- ConsoleApps/HataYönetimi/HataYönetimi/Program.cs
+++ ConsoleApps/HataYönetimi/HataYönetimi/Program.cs
@@ -54,25 +54,25 @@ namespace HataYönetimi
             #region TryCatchFinally
             //// Hata yönetimi için try-catch blokları kullanılır.
 
-            try
-            {
-                Console.WriteLine("İlk Sayıyı giriniz");
-                int number1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("İkinci sayıyı giriniz");
-                int number2 = Convert.ToInt32(Console.ReadLine());
-                int toplam = number1 + number2;
-                Console.WriteLine("İki sayının toplamı  " + toplam);
-            }
-            // Burada exception ex adında bir objeye dönüşüyor ve ex objesi içindeki işlemleri görebilir okuyabiliriz.
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            // Exception a girsin yada girmesin finally komutu çalışır.
-            finally
-            {
-                Console.WriteLine("Sonuna geldin");
-            }
+            //try
+            //{
+            //    Console.WriteLine("İlk Sayıyı giriniz");
+            //    int number1 = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("İkinci sayıyı giriniz");
+            //    int number2 = Convert.ToInt32(Console.ReadLine());
+            //    int toplam = number1 + number2;
+            //    Console.WriteLine("İki sayının toplamı  " + toplam);
+            //}
+            //// Burada exception ex adında bir objeye dönüşüyor ve ex objesi içindeki işlemleri görebilir okuyabiliriz.
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //// Exception a girsin yada girmesin finally komutu çalışır.
+            //finally
+            //{
+            //    Console.WriteLine("Sonuna geldin");
+            //}
 
             #endregion
         }
