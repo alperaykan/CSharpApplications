6ab1e0c7cca83e6542f4c0d3f34b2e09adaceadf
Alper Fırat Aykan <74792755+alperaykan@users.noreply.github.com>
2022-10-17 13:01:48 +0300

    ASP.NET CORE giriş

---
+++ HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Uygulama çalışıyorr...";
+        }
+    }
+}
