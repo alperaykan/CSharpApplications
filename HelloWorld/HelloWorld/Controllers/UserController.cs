189dd477a438a2e86efc04423e7052cf8ef55ef4
Alper Fırat Aykan <74792755+alperaykan@users.noreply.github.com>
2022-10-17 13:58:24 +0300

    ASP.net

---
+++ HelloWorld/HelloWorld/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    [Route("{controller}/{action}")]
+    public class UserController : Controller
+    {
+        public IActionResult GetUsers()
+        {
+            return View();
+        }
+    }
+}
